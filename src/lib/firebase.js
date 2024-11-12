7c26d29f1f291673cc4613cfb54e74e6979d2e32
Matke9 <matovicmihailo@gmail.com>
2024-11-12 13:43:37 +0100

    firebase.js update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -13,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
 const firestore = getFirestore(app);
 
-export { storage, firestore };
+export { firestore };

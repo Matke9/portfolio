da78bb3b3cf82c312c36626e8814b3b7074b0768
Matke9 <matovicmihailo@gmail.com>
2025-02-25 02:18:27 +0100

    logo i navbar promene

--- src/lib/VerticalNav.svelte
+++ src/lib/VerticalNav.svelte
@@ -1,28 +1,32 @@
 <script lang="ts">
+	type NavItem = {
+		href: string;
+		label: string;
+	};
 
-type NavItem = {
-    href: string;
-    label: string;
-};
-
-    export let navItems: NavItem[] = [];
+	export let navItems: NavItem[] = [];
 </script>
 
-<nav class="h-screen min-w-64 w-64 bg-[var(--dark-grey)] text-white flex flex-col pl-4 pr-4 space-y-4 justify-center z-0">
-    <img src="/LogoMMatovic.png" alt="mm" class="h-40 w-40 absolute top-3 left-3 bg-no-repeat bg-cover" />
-    <ul class="space-y-2">
-        {#each navItems as item}
-            <li class = "">
-                <a href="{item.href}" class="block p-2 rounded hover:bg-[var(--grey)] transition duration-200 ease-in-out">
-                    {item.label}
-                </a>
-            </li>
-        {/each}
-    </ul>
+<nav
+	class="z-0 flex h-screen w-56 min-w-56 flex-col justify-center space-y-4 bg-gradient-to-b from-[#848484] to-[#1E1E1E] pl-4 pr-4 text-white"
+>
+	<img src="/MMLogoBeli.png" alt="mm" class="absolute left-6 top-8 h-16 bg-cover bg-no-repeat" />
+	<ul class="space-y-2">
+		{#each navItems as item}
+			<li class="">
+				<a
+					href={item.href}
+					class="block rounded p-2 transition duration-200 ease-in-out hover:bg-[var(--grey)]"
+				>
+					{item.label}
+				</a>
+			</li>
+		{/each}
+	</ul>
 </nav>
 
 <style>
-    nav{
-        view-transition-name: nav;
-    }
+	nav {
+		view-transition-name: nav;
+	}
 </style>
